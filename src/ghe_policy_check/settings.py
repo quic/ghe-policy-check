6e07ce6de72e08ae9c639b1f702e80bc9ff148e2
Thomas Worley <quic_tworley@quicinc.com>
2022-07-28 17:26:44 -0700

    Remove security key
    Signed-off-by: Thomas Worley <quic_tworley@quicinc.com>

--- src/ghe_policy_check/settings.py
+++ src/ghe_policy_check/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "!n14wcmh5r&*5uo_(a$xaoelepan&0=bi^els)26v$f#$9!^s1"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
